76595ed92e7965dc5876789cae376750fb32fd14
Naveen George Thoppan <naveenjbl@gmail.com>
2015-04-16 14:04:34 +0530

    Initial Commit

--- TodoWatch/FirstViewController.swift
+++ TodoWatch/FirstViewController.swift
@@ -8,17 +8,59 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var todoList = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet var todoListtable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (NSUserDefaults.standardUserDefaults().objectForKey("todoList") != nil) {
+            
+           todoList = NSUserDefaults.standardUserDefaults().objectForKey("todoList") as [String]
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return todoList.count
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = todoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        todoListtable.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            
+            todoList.removeAtIndex(indexPath.row)
+            
+            NSUserDefaults.standardUserDefaults().setObject(todoList, forKey: "todoList")
+            
+            todoListtable.reloadData()
+        }
+    }
+
+
 
 
 }
--- TodoWatch/SecondViewController.swift
+++ TodoWatch/SecondViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var item: UITextField!
+    @IBAction func addItem(sender: AnyObject) {
+        
+        todoList.append(item.text)
+        
+        item.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(todoList, forKey: "todoList")
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +31,17 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) ->Bool {
+        
+        item.resignFirstResponder()
+        return true
     }
 
 
